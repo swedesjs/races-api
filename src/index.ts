341ef167307e42a458c619ef12ff33537718e12e
swedesjs <babaevvladislav828@gmail.com>
2021-07-26 20:03:38 +0300

    release 1..1.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch"
 import fastify from "fastify"
-import md5 from "md5"
+import crypto from "crypto"
 import { webhooksHandler, APIMethod } from "./api"
 import { APIError } from "./APIError"
 import { Response } from "./types"
@@ -11,7 +11,7 @@ export class Races {
   /**
    * @param token Для отправки любого запроса Вам понадобится access_token, его можно получить в разделе «{@link https://vk.com/app7679912 Прочее}», нажав на клавишу «{@link https://vk.com/app7679912 API}».
    */
-  constructor(public token: string) {
+  constructor(private token: string) {
     for (const method of MethodName) {
       this[method] = new Proxy(Object.create(null), {
         get:
@@ -21,7 +21,7 @@ export class Races {
               access_token: this.token,
               ...params
             })
-            let response: Response = await fetch(`https://race.danyarub.ru/api/${method}.${props}`, {
+            const response: Response = await fetch(`https://race.danyarub.ru/api/${method}.${props}`, {
               method: "POST",
               headers: {
                 "Content-type": "application/json"
@@ -40,14 +40,20 @@ export class Races {
   /**
    * Получить входящие платежи
    */
-  async startPollingPayment(handler: webhooksHandler) {
+  startPollingPayment(handler: webhooksHandler) {
     return new Promise(resolve => {
       const server = fastify()
       server.post("/", req => {
         const { userId, fieId, amount, id, sig } = req.body as Parameters<webhooksHandler>[0]
-        if (md5(this.token + amount + userId + id) === sig) handler({ userId, fieId, amount, id, sig })
+        if (
+          crypto
+            .createHash("md5")
+            .update(this.token + amount + userId + id)
+            .digest("hex") === sig
+        )
+          handler({ userId, fieId, amount, id, sig })
       })
-      server.listen(3000, async (err, url) => {
+      server.listen(3000, (err, url) => {
         if (err) {
           resolve(0)
           throw err
